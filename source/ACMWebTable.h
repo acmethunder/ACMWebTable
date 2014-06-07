3502564f0bfc4899ac14371f2c7f124e6801c584
Mike De Wolfe <dewolfe.michael@gmail.com>
2014-06-07 13:40:57 -0400

    Initial commit.

---
+++ source/ACMWebTable.h
@@ -0,0 +1,13 @@
+//
+//  ACMWebTable.h
+//  ACMWebView
+//
+//  Created by Michael De Wolfe on 2014-06-07.
+//  Copyright (c) 2014 acmethunder. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ACMWebTable : UIView
+
+@end
---
+++ source/ACMWebTable.m
@@ -0,0 +1,20 @@
+//
+//  ACMWebTable.m
+//  ACMWebView
+//
+//  Created by Michael De Wolfe on 2014-06-07.
+//  Copyright (c) 2014 acmethunder. All rights reserved.
+//
+
+#import "ACMWebTable.h"
+
+@implementation ACMWebTable
+
+- (instancetype)initWithFrame:(CGRect)frame {
+    if ( (self = [super initWithFrame:frame]) ) {
+        // Initialization code
+    }
+    return self;
+}
+
+@end
